8814b093cd66ab791cc79d9ac158b7606fcd4ff3
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-18 14:20:26 +0200

    ruta usuario individual

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IContact } from 'src/app/models/contact.interface';
 
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.scss']
+  styleUrls: ['./contact-page.component.scss'],
 })
-export class ContactPageComponent {
+export class ContactPageComponent implements OnInit {
+  contactList: IContact[] = [
+    {
+      id: 0,
+      name: 'John',
+      surname: 'Doe',
+      email: 'j@gmail.com',
+    },
+    {
+      id: 1,
+      name: 'Jane',
+      surname: 'Doe',
+      email: 'jane@gmail.com',
+    },
+    {
+      id: 2,
+      name: 'George',
+      surname: 'Smith',
+      email: 'gs@gmail.com',
+    },
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
