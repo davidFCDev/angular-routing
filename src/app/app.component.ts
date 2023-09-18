7d3c4b30289ca6b9dc2a8f58c04a21330bc79bf8
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-18 22:58:01 +0200

    logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-routing';
+
+  token: string | null = null;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.token = sessionStorage.getItem('token');
+  }
+
+  logoutUser() {
+    sessionStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
 }
