db8d57e15d17fb48aac3fde627433b887ac28a9e
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-18 15:38:23 +0200

    contact details

--- src/app/pages/contact-detail-page/contact-detail-page.component.ts
+++ src/app/pages/contact-detail-page/contact-detail-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-contact-detail-page',
   templateUrl: './contact-detail-page.component.html',
-  styleUrls: ['./contact-detail-page.component.scss']
+  styleUrls: ['./contact-detail-page.component.scss'],
 })
-export class ContactDetailPageComponent {
+export class ContactDetailPageComponent implements OnInit {
+  id: any | undefined;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: any) => {
+      if (params.id) {
+        this.id = params.id;
+      }
+    });
+  }
 }
