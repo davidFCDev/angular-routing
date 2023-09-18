cad42d20afa1313ae9c02559fc63d7517cbe82ac
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-18 13:28:52 +0200

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { ContactDetailPageComponent } from './pages/contact-detail-page/contact-detail-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    NotFoundPageComponent,
+    ContactPageComponent,
+    ContactDetailPageComponent
   ],
   imports: [
     BrowserModule,
