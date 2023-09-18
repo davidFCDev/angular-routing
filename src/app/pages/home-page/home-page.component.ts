a2cbaab4d7370a1b1f1ada0951ebbdeb95075230
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-18 14:11:36 +0200

    home page router

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {}
+
+  navigateToContacts(): void {
+    // this.router.navigate();
+  }
 }
