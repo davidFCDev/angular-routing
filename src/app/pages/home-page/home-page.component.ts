e2eee157fbd7e4ce896ed5e8f7cf01927e442ee2
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-19 13:50:50 +0200

    selected contact

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { IContact } from 'src/app/models/contact.interface';
 
 @Component({
   selector: 'app-home-page',
@@ -7,12 +8,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
+
+  token: string | null = null;
+  selectedContact: IContact | undefined;
+
   constructor(private router: Router) {}
 
   ngOnInit(): void {
+    // Comprobar si existe el token
+    this.token = sessionStorage.getItem('token');
+
     // Leemos del estado del historial de navegación
     if (history.state.data) {
       console.log(history.state.data);
+      this.selectedContact = history.state.data;
     }
   }
 
