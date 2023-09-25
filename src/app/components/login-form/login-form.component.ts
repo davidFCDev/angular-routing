25340b3f7e8ad5b9abbc7779b6b09cd796d4de4c
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-25 15:02:13 +0200

    ts ready

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login-form',
@@ -12,7 +13,7 @@ export class LoginFormComponent implements OnInit {
   loginForm: FormGroup = new FormGroup({});
 
   // Inyectamos el FormBuilder en el constructor
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {}
 
   ngOnInit() {
 
@@ -20,5 +21,23 @@ export class LoginFormComponent implements OnInit {
       email: ['', Validators.compose([Validators.required, Validators.email])],
       password: ['', Validators.required],
     });
+  }
+
+  // Creamos los getters para acceder a los campos del formulario
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  // Creamos el método onSubmit para enviar el formulario
+  onSubmit() {
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+      // TODO petición al servidor
+      this.loginForm.reset();
+    }
   }
 }
