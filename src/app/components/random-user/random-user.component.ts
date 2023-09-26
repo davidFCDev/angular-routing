3f1f7c872bc30a9420a5bd3cfe1dca18db754cca
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-26 14:17:45 +0200

    randomComp en page

--- src/app/components/random-user/random-user.component.ts
+++ src/app/components/random-user/random-user.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
-import { RandomUserService } from '../../services/random-user.service';
-import { IRandomContact, Results } from 'src/app/models/randomuser';
+import { IRandomContact } from 'src/app/models/randomuser';
 
 @Component({
   selector: 'app-random-user',
@@ -10,16 +9,12 @@ import { IRandomContact, Results } from 'src/app/models/randomuser';
 })
 export class RandomUserComponent implements OnInit {
 
-  randomResults: Results | undefined;
-  randomContact: IRandomContact | undefined;
+  @Input() randomContact: IRandomContact | undefined;
 
-  constructor(private randomUserService: RandomUserService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.randomUserService.getRandomContact().subscribe((response: Results) => {
-      this.randomContact = response.results[0];
-      console.table(this.randomContact);
-    });
+
   }
 
 }
--- src/app/pages/random-contact-page/random-contact-page.component.ts
+++ src/app/pages/random-contact-page/random-contact-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RandomUserService } from '../../services/random-user.service';
+import { IRandomContact, Results } from 'src/app/models/randomuser';
+
 
 @Component({
   selector: 'app-random-contact-page',
   templateUrl: './random-contact-page.component.html',
-  styleUrls: ['./random-contact-page.component.scss']
+  styleUrls: ['./random-contact-page.component.scss'],
 })
-export class RandomContactPageComponent {
+export class RandomContactPageComponent implements OnInit {
 
+  contact: IRandomContact | undefined;
+
+  constructor(private randomUserService: RandomUserService) { }
+
+  ngOnInit(): void {
+    this.randomUserService.getRandomContact().subscribe((response: Results) => {
+      this.contact = response.results[0];
+    });
+  }
 }
