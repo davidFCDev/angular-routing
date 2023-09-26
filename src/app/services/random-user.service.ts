4560c3114ed88d5d57afa2ffe86be22b027250f9
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-26 16:16:24 +0200

    handleerror

--- src/app/services/random-user.service.ts
+++ src/app/services/random-user.service.ts
@@ -1,21 +1,37 @@
 import { Injectable } from '@angular/core';
 
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 import { IRandomContact } from '../models/randomuser';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RandomUserService {
-
   constructor(private http: HttpClient) {}
 
+  handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('An error occurred:', error.error);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, body was: ${error.error}`
+      );
+    }
+    return throwError(
+      () => new Error('Something bad happened; please try again later.')
+    );
+  }
+
   getRandomContact(): Observable<any> {
-    return this.http.get('https://randomuser.me/api')
+    return this.http
+      .get('https://randomuser.me/api')
+      .pipe(catchError(this.handleError));
   }
 
   getRandomContacts(): Observable<IRandomContact[]> {
-    return this.http.get<IRandomContact[]>('https://randomuser.me/api/?results=10');
+    return this.http.get<IRandomContact[]>(
+      'https://randomuser.me/api/?results=10'
+    );
   }
 }
