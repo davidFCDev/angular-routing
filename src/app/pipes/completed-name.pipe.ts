8f18d1436e2a6ee9f028cd65001202906d397ceb
David Fernández <davidfernandezcomesana@gmail.com>
2023-09-27 14:28:40 +0200

    pipe corregido

--- src/app/pipes/completed-name.pipe.ts
+++ src/app/pipes/completed-name.pipe.ts
@@ -1,13 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { IContact } from '../models/contact.interface';
+import { IRandomContact } from '../models/randomuser';
 
 @Pipe({
   name: 'completedName'
 })
 export class CompletedNamePipe implements PipeTransform {
 
-  transform(contact: IContact, ...args: unknown[]): string {
-    return `${contact.name} ${contact.surname}`;
+  transform(contact: IRandomContact, ...args: unknown[]): string {
+    return `${contact.name.first} ${contact.name.last}`;
   }
 
 }
