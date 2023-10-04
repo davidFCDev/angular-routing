5330d7faaad1ffc25fddc6e3f7ce00a48eb8d7ee
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-04 22:09:13 +0200

    estilos task

--- src/app/components/kanban-tasks/kanban-tasks.component.ts
+++ src/app/components/kanban-tasks/kanban-tasks.component.ts
@@ -46,36 +46,36 @@ export class KanbanTasksComponent {
   ];
 
   doneTasks: ITask[] = [
-    {
-      title: 'Aprender html',
-      description: 'Aprender html',
-      completed: true,
-      level: LEVELS.INFO,
-    },
-    {
-      title: 'Aprender css',
-      description: 'Aprender css',
-      completed: true,
-      level: LEVELS.INFO,
-    },
-    {
-      title: 'Aprender sass',
-      description: 'Aprender sass',
-      completed: true,
-      level: LEVELS.INFO,
-    },
-    {
-      title: 'Aprender less',
-      description: 'Aprender less',
-      completed: true,
-      level: LEVELS.INFO,
-    },
-    {
-      title: 'Aprender stylus',
-      description: 'Aprender stylus',
-      completed: true,
-      level: LEVELS.INFO,
-    },
+    // {
+    //   title: 'Aprender html',
+    //   description: 'Aprender html',
+    //   completed: true,
+    //   level: LEVELS.INFO,
+    // },
+    // {
+    //   title: 'Aprender css',
+    //   description: 'Aprender css',
+    //   completed: true,
+    //   level: LEVELS.INFO,
+    // },
+    // {
+    //   title: 'Aprender sass',
+    //   description: 'Aprender sass',
+    //   completed: true,
+    //   level: LEVELS.INFO,
+    // },
+    // {
+    //   title: 'Aprender less',
+    //   description: 'Aprender less',
+    //   completed: true,
+    //   level: LEVELS.INFO,
+    // },
+    // {
+    //   title: 'Aprender stylus',
+    //   description: 'Aprender stylus',
+    //   completed: true,
+    //   level: LEVELS.INFO,
+    // },
   ];
 
   // todo = [
